23f3ab5e88e9c1db848f053d0e212e9faa60cae3
tjessica13 <jtanottawau@gmail.com>
2025-04-26 09:05:13 -0400

    adding next themes for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// adding dark mode
+import { ThemeProvider } from 'next-themes'; 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,12 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html suppressHydrationWarning lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
+      </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import { JetBrains_Mono } from 'next/font/google';
 
+import ThemeSwitch from "./ThemeSwitch";
+
 const jetBrains_Mono = JetBrains_Mono({ subsets: ['latin']});
 
 // get the current date
@@ -10,13 +12,14 @@ const date = new Date();
 export default function Home() {
   return (
     <div className={`${jetBrains_Mono.className} m-5`}>
+      <ThemeSwitch></ThemeSwitch>
       <div className="flex justify-between">
         <ul>
-          <li><a href="" className="hover:ml-12 hover:text-black text-(--fontcolor)">PROFESSIONAL EXPERIENCE \</a></li>
-          <li><a href="" className="hover:ml-12 hover:text-black text-(--fontcolor)">EDUCATION \</a></li>
-          <li><a href="" className="hover:ml-12 hover:text-black text-(--fontcolor)">SKILLS \</a></li>
-          <li><a href="" className="hover:ml-12 hover:text-black text-(--fontcolor)">PROJECTS \</a></li>
-          <li><a href="" className="hover:ml-12 hover:text-black text-(--fontcolor)">CONTACT \</a></li>
+          <li><a href="" className="hover:ml-12 hover:text-(--hovercolor) text-(--fontcolor)">PROFESSIONAL EXPERIENCE \</a></li>
+          <li><a href="" className="hover:ml-12 hover:text-(--hovercolor) text-(--fontcolor)">EDUCATION \</a></li>
+          <li><a href="" className="hover:ml-12 hover:text-(--hovercolor) text-(--fontcolor)">SKILLS \</a></li>
+          <li><a href="" className="hover:ml-12 hover:text-(--hovercolor) text-(--fontcolor)">PROJECTS \</a></li>
+          <li><a href="" className="hover:ml-12 hover:text-(--hovercolor) text-(--fontcolor)">CONTACT \</a></li>
         </ul>
         <p className="date">{date.getFullYear()}:{date.getMonth()+1}:{date.getDate()}</p>
       </div>
