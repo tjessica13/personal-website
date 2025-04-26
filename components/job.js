5b5701ed832718bbebdb061cbeef60891432a7e7
tjessica13 <jtanottawau@gmail.com>
2025-04-26 18:09:40 -0400

    complete style for experience page

--- components/job.js
+++ components/job.js
@@ -2,12 +2,13 @@
 const Job = ({id, title, company, dates, points}) => {
     return (
         <div className="flex p-15">
-            <div className="flex-1">
+            <div className="flex-1 border-solid border-r border-(--fontcolor)">
                 <h1>{title}</h1>
+                <p>Co-op</p>
                 <h3>{company}</h3>
                 <p>{dates}</p>
             </div>
-            <ul className="list-disc flex-1 color-(--fontcolor)">
+            <ul className="list-disc flex-1 color-(--fontcolor) m-8">
                 {points.map((point, index) => (
                     <li className="ml-6" key={index}><p>{point}</p></li>
                 ))}
