6ff27b51ecb3b03fe96466818a2129c6fc75f8ab
tjessica13 <jtanottawau@gmail.com>
2025-04-28 17:34:13 -0400

    fixing p tags

--- components/job.js
+++ components/job.js
@@ -4,9 +4,9 @@ const Job = ({id, title, company, dates, points}) => {
         <div className="p-5 sm:flex sm:p-15">
             <div className="sm:flex-1 sm:border-solid sm:border-r sm:border-(--fontcolor) p-5">
                 <h1 className="text-2xl sm:text-4xl">{title}</h1>
-                <p>Co-op</p>
+                <h3>Co-op</h3>
                 <h3>{company}</h3>
-                <p>{dates}</p>
+                <h3>{dates}</h3>
             </div>
             <ul className="list-disc flex-1 color-(--fontcolor) m-8">
                 {points.map((point, index) => (
