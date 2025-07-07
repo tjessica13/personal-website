2913b671f7b1c9c97aba26c29b009f9250e018db
tjessica13 <jtanottawau@gmail.com>
2025-07-07 14:28:56 -0400

    changing contect page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 import DateComponent from '@/components/date';
 import ThemeSwitch from '../ThemeSwitch';
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Contact() {
     return (
@@ -12,9 +13,13 @@ export default function Contact() {
                 </div>
             </div>
             
-            <h1 className="text-center text-xl">jessicatan.business@gmail.com</h1>
-            <a href="https://www.linkedin.com/in/t-jessica"><h1 className="text-center text-xl">https://www.linkedin.com/in/t-jessica/</h1></a>
-            <a href="https://github.com/tjessica13"><h1 className="text-center text-xl">https://github.com/tjessica13</h1></a>
+            <div className="mt-60">
+                <h1 className="text-center text-2xl">jessicatan.business@gmail.com</h1>
+                <div className="flex justify-center gap-5 m-5">
+                    <a href="https://www.linkedin.com/in/t-jessica"><FaLinkedin className="icon"/></a>
+                    <a href="https://github.com/tjessica13"><FaGithub className="icon"/></a>
+                </div>
+            </div>
         </div>
     )
 }
