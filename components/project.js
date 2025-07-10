b38f2d869a153c40b89f90f0d90ddf765e3fe4db
tjessica13 <jtanottawau@gmail.com>
2025-07-09 20:23:20 -0400

    modify projects page

--- components/project.js
+++ components/project.js
@@ -1,15 +1,17 @@
-import { FaRegFolder, FaRegWindowClose } from "react-icons/fa";
+import { FaRegFolder } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 
 const ProjectComponent = ({title, dates, github, points}) => {
     return (
         <div className="sm:flex justify-center w-full m-10">
-            <div className="w-xs mr-10 sm:place-items-center"><FaRegFolder size={40} className="icon"/><div><h3 className="sm:text-center">{title}</h3></div></div>
+            <div className="w-xs mr-10 sm:place-items-center">
+                <a href={github} className="flex justify-center"><FaRegFolder size={40} className="icon"/></a>
+                <div><h3 className="sm:text-center">{title}</h3></div>
+            </div>
             <div className="w-xs sm:w-2xl border border-solid border-(--fontcolor)">
                 <div className="flex justify-between items-center border-b border-solid border-(--fontcolor) 
                 w-full p-2">
                     <h3>{title}</h3>
-                    <a href="./"><FaRegWindowClose size={40} className="icon"></FaRegWindowClose></a>
                 </div>
                 <div className="p-5">
                     <h3>{dates}</h3>
